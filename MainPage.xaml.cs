cddacb6f0548c5303db9b46441ec09a097e7c4e1
donald huang <donald_huang@dell.com>
2019-04-01 11:23:46 +0800

    First edition

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyUWP.ContentPages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +27,59 @@ namespace MyUWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void NavView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+
+            switch (navItemTag)
+            {
+                case "EditPage":
+                    _page = typeof(EditPage);
+                    break;
+                case "PublishPage":
+                    _page = typeof(PublishPage);
+                    break;
+                case "TransCodePage":
+                    _page = typeof(TransCodePage);
+                    break;
+                case "settings":
+                    _page = typeof(SettingsPage);
+                    break;
+                case "RecordPage":
+                default:
+                    _page = typeof(RecordPage);
+                    break;
+            }
+
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            var preNavPageType = contentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                contentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private void NvSample_Loaded(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(typeof(RecordPage));
+        }
+
+        private void NvSample_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
         }
     }
 }
